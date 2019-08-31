930e4d8c7048cdce2522abe1cd64c345adf7e17a
kwanwoo0122 <kwanwoo0122@naver.com>
2019-08-31 23:38:05 +0900

    Initial commit

--- 메인페이지/sideBar.js
+++ 메인페이지/sideBar.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-  $('#sidebarCollapse').on('click', function () {
-    $('#sidebar').toggleClass('active');
+  $('.sidebarCollapse').on('click', function () {
+    $('.sidebar').toggleClass('active');
   });
 
 });
